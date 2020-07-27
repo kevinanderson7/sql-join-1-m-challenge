73ecff6b396499ef5392809208743d8eba2ad339
Kevin Anderson <kmichael419@hotmail.com>
2020-07-27 16:36:28 -0500

    question 5 complete

--- assignment.sql
+++ assignment.sql
@@ -3,19 +3,23 @@
 -- Each query should have 10 or less rows as a result.
 
 -- 1. Display the total number of countries in the country table.
-
+SELECT count(*) FROM "country";
 
 -- 2. Display the entire world population (research SUM). 
-
+SELECT SUM(population) FROM "country";
 
 -- 3. Displays the name and population for the 10 countries with the highest population.
-
+SELECT "name", "population" FROM "country" ORDER BY "population" DESC;
 
 -- 4. Displays the number of countries in each region.
-
+SELECT "region".name, count(*) FROM "region"
+JOIN "country" ON "region".id = "country".region_id
+GROUP BY "region".name;
 
 -- 5. Displays the total population for each region.
-
+SELECT "region".name, SUM(population) FROM "region"
+JOIN "country" ON "region".id = "country".region_id
+GROUP BY "region".name;
 
 -- 6. Display the name and sq km for the 10 countries with the most land area (sort by land area).
 
